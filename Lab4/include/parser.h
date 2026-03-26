74519d6907d3777a94b9be14b8250e679f6afbd9
Gabi <gabriel.balan@isa.utm.md>
2026-03-26 03:34:32 +0200

    created Parser interface

--- Lab4/include/parser.h
+++ Lab4/include/parser.h
@@ -3,9 +3,25 @@
 
 #include "lexer.h"
 #include "token.h"
+#include "nodes.h"
+#include "token.h"
 
 class Parser
 {
+    private:
+    Lexer& m_lexer;
+    Token m_currentToken{};
+    void consume(TokenType expectedType);
+
+    public:
+    Parser(Lexer& lexer) : m_lexer{lexer}
+    {
+        m_currentToken = m_lexer.getNextToken();
+    }
+    Node* parseExpression(); // OR
+    Node* parseSequence();   // CONCAT
+    Node* parseFactor();     // ?, *, +, ^nr
+    Node* parseBase();       // literal
 
 };
 
